79c3bcdcc353a73fb99546efa1d6b3f77a8e16a7
frukanfed <frukansalih@gmail.com>
2026-07-16 16:09:13 +0300

    change to args

--- src/main/java/cli/Main.java
+++ src/main/java/cli/Main.java
@@ -7,28 +7,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        commandLoop:
-        while (scan.hasNextLine()) {
-            String input = scan.nextLine();
-
-            switch (input) {
-                case "init":
-                    new InitCommand().execute();
-                    break;
-                case "status":
-                    new StatusCommand().execute();
-                    break;
-                case "q":
-                case "quit":
-                    System.out.println("Exiting program...");
-                    break commandLoop;
-                default:
-                    System.out.println("Invalid command: " + input);
-                    break;
-            }
+        if (args.length == 0) {
+            System.err.println("No command provided");
+            System.exit(1);
         }
 
+        String command = args[0];
+        if (!command.equals("init") && !command.equals("status")) {
+            System.err.println("Invalid command");
+            System.exit(1);
+        }
+
+        switch (command) {
+            case "init":
+                new InitCommand().execute();
+                break;
+            case "status":
+                new StatusCommand().execute();
+                break;
+            default:
+                System.err.println("Invalid command");
+                break;
+        }
     }
 }
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        commandLoop:
+//        while (scan.hasNextLine()) {
+//            String input = scan.nextLine();
+//
+//            switch (input) {
+//                case "init":
+//                    new InitCommand().execute();
+//                    break;
+//                case "status":
+//                    new StatusCommand().execute();
+//                    break;
+//                case "q":
+//                case "quit":
+//                    System.out.println("Exiting program...");
+//                    break commandLoop;
+//                default:
+//                    System.out.println("Invalid command: " + input);
+//                    break;
+//            }
+//        }
+//
+//        scan.close();
