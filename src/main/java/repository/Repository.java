01fb33715ae5d4e30091ee28e7b113e88baaf159
frukanfed <frukansalih@gmail.com>
2026-07-17 15:31:02 +0300

    create folder/file

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -1,22 +1,32 @@
 package repository;
 
+import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.Files;
 
 
 public class Repository {
-    private static final Path REPOSITORY_PATH = Paths.get(".jgit");
+    private static final Path REPOSITORY_PATH = Path.of(".jgit");
 
     public boolean exists() {
-        return Path.of(REPOSITORY_PATH.toString()).toFile().exists();
+        return Files.exists(REPOSITORY_PATH);
     }
 
     public void init() {
         if (exists()) {
             throw new RuntimeException("Repository already exists");
-        } else {
-            System.out.println("Repository created");
+        }
+
+        try {
+            Files.createDirectory(REPOSITORY_PATH);
+
+            Files.createDirectory(REPOSITORY_PATH.resolve("objects"));
+            Files.createDirectories(REPOSITORY_PATH.resolve("refs/heads/main"));
+
+            Files.writeString(REPOSITORY_PATH.resolve("HEAD"), "ref: refs/heads/main");
+            Files.createFile(REPOSITORY_PATH.resolve("index"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to initialize " + e.getMessage(), e);
         }
     }
 }
---
+++ src/main/java/util/Hashing.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Hashing {
+    public String sha1(byte[] bytes) {
+
+    }
+}
