cec30d6001f0185d6d40be9735e36669a054f1e2
frukanfed <frukansalih@gmail.com>
2026-07-17 15:50:43 +0300

    hashing

--- src/main/java/util/Hashing.java
+++ src/main/java/util/Hashing.java
@@ -1,7 +1,23 @@
 package util;
 
-public class Hashing {
-    public String sha1(byte[] bytes) {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+public class Hashing {
+    public static String sha1(byte[] bytes) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            byte[] messageDigest = md.digest(bytes);
+
+            StringBuilder builder = new StringBuilder(40);
+            for (byte b: messageDigest) {
+                builder.append(String.format("%02x", b));
+            }
+
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-1 algorithm unavailable", e);
+        }
     }
 }
