01b21ec38ac661d8f99477816443e3ea4c8d0ba4
Frukanfed <frukansalih@gmail.com>
2026-07-23 15:00:12 +0300

    add 2

--- src/main/java/storage/IndexStore.java
+++ src/main/java/storage/IndexStore.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Objects;
 
 public class IndexStore {
     private final Repository repository;
@@ -15,14 +17,29 @@ public class IndexStore {
     }
 
     public void add(String filePath, String hash) {
-        String content = filePath + " " + hash + "\n";
+        String content = filePath + " " + hash;
+        Path indexDir = repository.getIndexDirectory();
+        boolean found = false;
 
         try {
-            Files.writeString(
-                    repository.getIndexDirectory(),
-                    content,
-                    StandardOpenOption.APPEND
-            );
+            List<String> lines = Files.readAllLines(indexDir);
+
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
+
+                String[] text = line.split(" ");
+
+                if (Objects.equals(filePath, text[0])){
+                    lines.set(i, content);
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                lines.add(content);
+            }
+
+            Files.write(indexDir, lines);
         } catch (IOException e) {
             throw new RuntimeException("Failed to index: " + e.getMessage(), e);
         }
